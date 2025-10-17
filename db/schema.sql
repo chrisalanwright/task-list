65631197a6dd791f32059b8307a41caa0851dbde
Christopher Wright <chrisalanwright@gmail.com>
2025-10-16 22:36:40 -0400

    set-up-schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
 
+CREATE TABLE users (
+    id serial PRIMARY KEY,
+    username text UNIQUE NOT NULL,
+    password text NOT NULL
+);
+
+CREATE TABLE tasks (
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    done boolean NOT NULL,
+    user_id integer NOT NULL REFERENCES user(id) ON DELETE CASCADE
+);
