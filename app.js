e363a853e8494ca45449b5d70d6ab0b566c7558e
Christopher Wright <chrisalanwright@gmail.com>
2025-10-27 14:24:09 -0400

    added-all-imports-to-app.js-and-passes-tests

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 import express from "express";
 const app = express();
-export default app;
+
+import tasksRouter from "#api/tasks";
+import usersRouter from "#api/users";
+import getUserFromToken from "#middleware/getUserFromToken";
+
+app.use(express.json());
+
+app.use(getUserFromToken);
+
+app.use("/tasks", tasksRouter);
+app.use("/users", usersRouter);
 
 app.use((err, req, res, next) => {
   switch (err.code) {
@@ -21,3 +31,5 @@ app.use((err, req, res, next) => {
   console.error(err);
   res.status(500).send("Sorry! Something went wrong.");
 });
+
+export default app;
